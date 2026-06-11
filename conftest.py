433adcb6c9855c5772a17d1bb9a40a7e81dcc652
karthik <kishorkarthik777@gmail.com>
2026-06-11 19:53:05 +0530

    Conftest corection

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture(scope="function")
 def page():
     with sync_playwright() as p:
-        browser=p.chromium.launch(headless=False)
+        browser=p.chromium.launch(headless=True)
         context =browser.new_context()
         page = context.new_page()
         page.set_default_timeout(60000)
